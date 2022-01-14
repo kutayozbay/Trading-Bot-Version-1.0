703da20c5b3d09b92dee9a2e741cc696b2a6d028
kutayozbay <kutayozbay00@gmail.com>
2022-01-14 17:49:10 +0300

    Code and Readme has added

---
+++ README.md
@@ -0,0 +1,2 @@
+# Trading Bot Version 1.0
+ Real time trading bot using Binance API
---
+++ config.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 17 16:14:55 2021
+
+@author: user
+"""
+API_KEY = "Your API_KEY"
+API_SECRET = "Your API_SECRET"
